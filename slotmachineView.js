d7062fe9e975d1e38e61df075cff06517f00961e
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-01 14:15:52 -0400

    logic for a random image to appear in each slot from images folder

--- slotmachineView.js
+++ slotmachineView.js
@@ -7,14 +7,18 @@ class MusicSlot {
     const slotMachine = document.createElement('div');
     slotMachine.setAttribute('id', 'slotMachine');
     document.body.appendChild(slotMachine);
-
+    let slotImage = ['images/sbtrkt.jpg', 'images/daftpunk.jpg', 'images/adele.jpg', 'images/michaelJackson.jpg', 'images/rihanna.jpg', 'images/tycho.jpg', 'images/fkatwigs.jpg', 'images/depechemode.jpg', 'images/coldplay.jpg', 'images/xx.jpg', 'images/kaiydo.jpg', 'images/feverray.jpg'];
     for (let i = 0; i < 9; i++) {
       let slotElement = document.createElement('div');
       slotElement.setAttribute('class', 'slot');
+      let num=  Math.floor(Math.random()*slotImage.length);
+      let img = slotImage[num];
+      slotElement.innerHTML = '<img src="' + img + '" width="144px">';
       slotMachine.appendChild(slotElement);
     }
   }
-}
+  }
+
 
 let newMachine = new MusicSlot();
 newMachine.createSlotMachine();
